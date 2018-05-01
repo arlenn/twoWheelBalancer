e6bc85f55b580de5ce451839f5fc6ea90421c6e7
DESKTOP-ALTQC0F\hardyn <arlen.nelson@gmail.com>
2018-05-01 12:03:43 -0700

    More headers, reduces UART priority

--- uart.c
+++ uart.c
@@ -38,6 +38,18 @@ double kc = 0;
 
 #define MAX_STR_LEN 24
 
+/************************************************************************************
+ * Function: uartInit
+ * configure GPIO as UART for Bluetooth comms.
+ * configured as lowest priorty task
+
+ * argument: void
+ * return: success/failure
+ * Author: Hardy Nelson
+ * Date:
+ * Revision:
+ *************************************************************************************/
+
 uint8_t uartInit(void) {
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
@@ -49,17 +61,27 @@ uint8_t uartInit(void) {
     GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
     UARTClockSourceSet(UART1_BASE, UART_CLOCK_PIOSC); //16Mhz crystal oscillator
-    UARTStdioConfig(1, 38400, 16000000);  //port 0, baud-rate, 16MHz oscillator magic number
+    UARTStdioConfig(1, 38400, 16000000);  //port 1, baud-rate, 16MHz oscillator magic number
+
+    IntPrioritySet(INT_UART1, 0xE0)  //lowest priority
 
     IntEnable(INT_UART1); //enable the UART interrupt
 
     UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT); //only enable RX and TX interrupts
 
-    //UARTIntEnable(UART1_BASE, UART_INT_RX); //only enable RX and TX interrupts
-
     return EXIT_SUCCESS;
 }
 
+/************************************************************************************
+ * Function: UartGetK
+ * text parser from UART for robot control
+
+ * argument: void
+ * return: void
+ * Author: Hardy Nelson
+ * Date:
+ * Revision:
+ *************************************************************************************/
 
 void UartGetK(void){
 
@@ -132,6 +154,17 @@ void UartGetK(void){
         UARTprintf("command not recognized\n");
     }
 }
+
+/************************************************************************************
+ * Function: UARTIntHandler
+ * ISR for UART Rx
+
+ * argument: void
+ * return: void
+ * Author: Hardy Nelson
+ * Date:
+ * Revision:
+ *************************************************************************************/
 
 void UARTIntHandler(void)
 {
